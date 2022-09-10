f4e331b5a4228812278e12275244be837cee2706
ztjssw <110404406+ztjssw@users.noreply.github.com>
2022-09-10 15:01:34 +0800

    Update Busuu.js

--- Busuu.js
+++ Busuu.js
@@ -3,8 +3,8 @@
 解锁博树Busuu会员
 更新日期:2022.09.10
 [rewrite_local]
-^https?:\/\/api.busuu\.com\/users\/me url script-response-body https://raw.githubusercontent.com/ztjssw/Quantumultx/main/Busuu.js
+^https?:\/\/*\/users\/me url script-response-body https://raw.githubusercontent.com/ztjssw/Quantumultx/main/Busuu.js
 [mitm]
-hostname = api.busuu.com
+hostname = *
 */
 var body = $response.body.replace(/tier":"\w+/g, 'tier":"plus').replace(/expiration":\w+/g, 'expiration":1820845855'); $done({body});
