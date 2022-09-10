0c2b460cc404369026fe387a3cbee96d7eb9cb9e
ztjssw <110404406+ztjssw@users.noreply.github.com>
2022-09-10 14:42:16 +0800

    Update Busuu.js

--- Busuu.js
+++ Busuu.js
@@ -3,7 +3,7 @@
 解锁博树Busuu会员
 更新日期:2022.09.10
 [rewrite_local]
-^https?:\/\/api.busuu\.com\/users\/me url script-response-body
+^https?:\/\/api.busuu\.com\/users\/me url script-response-body https://raw.githubusercontent.com/ztjssw/Quantumultx/main/Busuu.js
 [mitm]
 hostname = api.busuu.com
 */
