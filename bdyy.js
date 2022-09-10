572cccbba60f97ddcb1e5fedd81afe46eda8461d
ztjssw <110404406+ztjssw@users.noreply.github.com>
2022-09-10 16:14:54 +0800

    Update bdyy.js

--- bdyy.js
+++ bdyy.js
@@ -3,7 +3,7 @@
 更新日期:2022.09.09
 波点音乐解锁vip
 [rewrite_local]
-^http[s]:\/\/(.*.kuwo.*) url script-response-body
+^http[s]:\/\/(.*.kuwo.*) url script-response-body https://raw.githubusercontent.com/ztjssw/Quantumultx/main/bdyy.js
 [mitm]
 hostname = bd-api.kuwo.cn
 */ 
