9e51cc101cad751a006f215157524099e0dda506
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-09-17 14:26:02 +0800

    Update dtpk.js

--- dtpk.js
+++ dtpk.js
@@ -3,7 +3,7 @@
 地铁跑酷解锁钥匙，滑板，金币，(没效果app卸载重装)
 日期:2022.09.17
 [rewrite_local]
-^https:\/\/(.*.subway.uu.cc)\/backup\/readBasicData url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/dtpk.js
+^https:\/\/game\.subway\.uu\.cc\/backup\/readBasicData url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/dtpk.js
 hostname = game.subway.uu.cc
 **************************************/
 var body = $response.body.replace(/coins":"\d+/g, 'coins":"987654321').replace(/keys":"\d+/g, 'keys":"123456').replace(/skids":"\d+/g, 'skids":"78910'); $done({body});
