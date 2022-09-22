9654608fddb28686f292d679c0fae0071b959ea3
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-09-23 01:44:55 +0800

    Update bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -3,6 +3,6 @@
 b站解锁大会员番剧影视
 日期:2022.09.22
 [rewrite_local]
-^http[s]:\/\/(.*.bili*.*) url script-request-header https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/bi.js
-hostname = *.bili*.*
+^http[s]:\/\/(.*.bili*.com) url script-request-header https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/bi.js
+hostname = *.bili*.com
 *********************************************/
