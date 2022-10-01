d8cdde9fcd4a9274956b3e2d66f984cb6a27ddeb
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-02 00:02:48 +0800

    Update bsq.js

--- bsq.js
+++ bsq.js
@@ -1,6 +1,6 @@
 /*************************************
 @@Zoo
-变声器变声吧，恢复订阅
+变声器变声吧，恢复订阅(没效果就试用，谢谢您)
 日期:2022.10.1
 [rewrite_local]
 ^https:\/\/voicechanger\.szsszykj\.com\/interface\/GetIosVip\.php url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/bsq.js
