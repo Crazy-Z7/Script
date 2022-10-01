985e3027ffa712a8b30a864ec9537030fc5c3059
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-01 23:19:30 +0800

    Update and rename biansq.js to bsq.js

--- bsq.js
+++ bsq.js
@@ -3,6 +3,6 @@
 变声器变声吧，恢复订阅(需要用的时候开规则进入)
 日期:2022.10.1
 [rewrite_local]
-^https:\/\/voicechanger\.szsszykj\.com\/interface\/GetIosVip\.php url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/biansq.js
+^https:\/\/voicechanger\.szsszykj\.com\/interface\/GetIosVip\.php url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/bsq.js
 hostname = voicechanger.szsszykj.com
 **************************************/
