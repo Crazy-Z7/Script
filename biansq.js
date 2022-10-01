c902106bcf1c8c92c6f72302cbd4e672fd50aa67
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-01 23:16:44 +0800

    Update biansq.js

--- biansq.js
+++ biansq.js
@@ -6,4 +6,3 @@
 ^https:\/\/voicechanger\.szsszykj\.com\/interface\/GetIosVip\.php url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/biansq.js
 hostname = voicechanger.szsszykj.com
 **************************************/
-var body = $response.body.replace(/.+/g, '"{\n  \"Header\": {\n    \"Result\": 0,\n    \"Msg\": \"查询成功\"\n  },\n  \"Content\": {\n    \"system_time\": \"2022-10-01 22:54:59\",\n    \"end_time\": \"9999-09-09 15:28:24\",\n    \"is_vip\": 1\n  }\n}"'); $done({body});
