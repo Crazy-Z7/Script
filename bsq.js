da16ad8698e21387e3b82961b3a915929ee20d03
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-01 23:24:32 +0800

    Update bsq.js

--- bsq.js
+++ bsq.js
@@ -6,4 +6,3 @@
 ^https:\/\/voicechanger\.szsszykj\.com\/interface\/GetIosVip\.php url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/bsq.js
 hostname = voicechanger.szsszykj.com
 **************************************/
-var body = $response.body.replace(/end_time":".*?"/g, 'end_time":"2032-09-21 15:28:24"').replace(/is_vip":\d/g, 'is_vip":1'); $done({body});
