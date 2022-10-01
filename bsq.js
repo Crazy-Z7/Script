80a65a9ae6bcd0212f31aef3f3da1a16ab160165
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-01 23:53:05 +0800

    Delete bsq.js

--- bsq.js
+++
@@ -1,8 +0,0 @@
-/*************************************
-@@Zoo
-变声器变声吧，恢复订阅(需要用的时候开规则进入)
-日期:2022.10.1
-[rewrite_local]
-^https:\/\/voicechanger\.szsszykj\.com\/interface\/GetIosVip\.php url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/bsq.js
-hostname = voicechanger.szsszykj.com
-**************************************/
