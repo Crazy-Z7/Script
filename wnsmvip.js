f33452be02576a1fd5841e534f8691b9f8f9e0c5
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-03 22:59:43 +0800

    Update wnsmvip.js

--- wnsmvip.js
+++ wnsmvip.js
@@ -3,6 +3,7 @@
 蜗牛睡眠解锁vip付费音频
 日期:2022.10.03
 [rewrite_local]
+^https:\/\/music\.snailsleep\.net\/snail-music\/music\/natural\/sound\/list url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/wnsm1.js
 ^https:\/\/music\.snailsleep\.net url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/wnsm.js
 hostname = music.snailsleep.net
 **************************************/
