57f53a5079331174f037a2595129c921f8f0292c
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-03 21:11:44 +0800

    Update wscsds.js

--- wscsds.js
+++ wscsds.js
@@ -6,15 +6,4 @@
 ^https:\/\/iap\.etm\.tech\/receipts url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/wscsds.js
 hostname = iap.etm.tech
 **************************************/
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const wscsds = '/receipts';
-
-if (ua.indexOf('wscsds') != -1) {
-    Body = {"{\n  \"entitlements\": [{\n    \"expires_date_ms\": 4739583083000,\n    \"purchase_date_ms\": 1655289297000,\n    \"product_identifier\": \"SpeedTest_RemoveAd_1_Year_20181015\",\n    \"is_in_trial_period\": false,\n    \"is_in_intro_offer_period\": false,\n    \"environment\": \"Production\",\n    \"redeem\": {},\n    \"auto_renew\": true,\n    \"entitlement_id\": \"premium\"\n  }],\n  \"is_valid\": true\n}"}};
-
-    body = JSON.stringify(obj);
-}
-$done({body});
+var body = $response.body.replace(/.+/g, '"{\n  \"entitlements\": [{\n    \"expires_date_ms\": 4739583083000,\n    \"purchase_date_ms\": 1655289297000,\n    \"product_identifier\": \"SpeedTest_RemoveAd_1_Year_20181015\",\n    \"is_in_trial_period\": false,\n    \"is_in_intro_offer_period\": false,\n    \"environment\": \"Production\",\n    \"redeem\": {},\n    \"auto_renew\": true,\n    \"entitlement_id\": \"premium\"\n  }],\n  \"is_valid\": true\n}"'); $done({body});
