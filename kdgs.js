a54f44b006ebb7e9cd585b9ebd3ee3b8370918d8
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-15 21:15:56 +0800

    Delete kdgs.js

--- kdgs.js
+++
@@ -1,9 +0,0 @@
-/*************************************
-@@Zoo
-
-日期:2022.10.02
-[rewrite_local]
-^https?:\/\/service.hhdd.com url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/kdgs.js
-hostname = service.hhdd.com
-**************************************/
-var body = $response.body.replace(/subscribeStatus":\d/g, 'subscribeStatus":0').replace(/bookVip":\d/g, 'bookVip":0').replace(/vipStatus":\d/g, 'vipStatus":1').replace(/endTime":\d/g, 'endTime":9999-09-09'); $done({body});
