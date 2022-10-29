fc279f2a42ed664d04606040a0b5282f6129abbb
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-29 15:54:33 +0800

    Update pzst.js

--- pzst.js
+++ pzst.js
@@ -4,6 +4,6 @@
 日期:2022.10.29
 [rewrite_local]
 ^http[s]?:\/\/appss\.rhinoxlab\.com\/app\/account\/getAccountInfo.+$ url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/pzst.js
-hostname = *.rhinoxlab.*
+hostname = *
 **********************************************/
 var body = $response.body.replace(/.+/g, '"{\n  \"success\": true,\n  \"returnCode\": \"200\",\n  \"result\": {\n    \"nickname\": \"U2210562808\",\n    \"headImg\": \"https:\/\/boniuapp.rhinox.cn\/common\/head\/1599546812784.png\",\n    \"type\": \"VIP\",\n    \"vipExpireTime\": \"9999-09-09 15:20:32\",\n    \"vipExpireDays\": 3,\n    \"times\": 0,\n    \"dataId\": \"10166702792813965690\",\n    \"vipGroupInfos\": [{\n      \"groupType\": \"TYPE_ONE\",\n      \"vipType\": \"VIP\",\n      \"autoPay\": \"YES\"\n    }],\n    \"remainTimeSeconds\": 0,\n    \"realnameStatus\": \"NO\",\n    \"wordage\": 0\n  },\n  \"timeOut\": false\n}"'); $done({body});
