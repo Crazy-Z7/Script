1a4e4acf834e80047a5e35347116084bebd8da67
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-30 20:21:40 +0800

    Create quting.js

---
+++ quting.js
@@ -0,0 +1,8 @@
+/**********************************************
+@@Zoo
+趣听音乐屏蔽升级弹窗
+日期:2022.10.30
+[rewrite_local]
+^https:\/\/api\.bspapp\.com\/client url reject-dict
+hostname = api.bspapp.com
+**********************************************/
