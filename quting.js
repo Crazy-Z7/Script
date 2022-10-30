11236517dbd3fc6f6a82a1d0b5020e10720b5792
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-10-31 07:51:19 +0800

    Delete quting.js

--- quting.js
+++
@@ -1,8 +0,0 @@
-/**********************************************
-@@Zoo
-趣听音乐屏蔽升级弹窗
-日期:2022.10.30
-[rewrite_local]
-^https:\/\/api\.bspapp\.com\/client url reject-dict
-hostname = api.bspapp.com
-**********************************************/
