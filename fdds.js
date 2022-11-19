61dd6d4a21cf56827a850aa5aed491dc70d4d118
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-11-19 17:02:11 +0800

    Update fdds.js

--- fdds.js
+++ fdds.js
@@ -3,7 +3,6 @@
 樊登读书解锁会员权限
 日期:2022.11.19
 [rewrite_local]
-^https?:\/\/(.*.dushu365.com) url script-request-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/fdds.js
+^https?:\/\/(.*.dushu365.com) url request-body "token":"[^"]+ request-body "token":"20221119qbmqdUt8vptKBeZn7ox
 hostname = *.dushu365.com
 **********************************************/
-var body = $request.body.replace(/"token":".*?"/g, '"token":"20221119qbmqdUt8vptKBeZn7ox"'); $done({body});
