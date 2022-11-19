4a77909543ab0305ffd9eadb5f3eea4a0a37a857
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-11-19 17:07:44 +0800

    Delete fdds.js

--- fdds.js
+++
@@ -1,8 +0,0 @@
-/**********************************************
-@@Zoo
-樊登读书解锁会员权限
-日期:2022.11.19
-[rewrite_local]
-^https?:\/\/(.*.dushu365.com) url request-body "token":"[^"]+ request-body "token":"20221119qbmqdUt8vptKBeZn7ox
-hostname = *.dushu365.com
-**********************************************/
