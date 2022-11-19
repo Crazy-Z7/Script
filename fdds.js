de271228aa7f40fe7c3a35bf06977c14f7ac43a0
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-11-19 16:52:18 +0800

    Delete fdds.js

--- fdds.js
+++
@@ -1,9 +0,0 @@
-/**********************************************
-@@Zoo
-樊登读书解锁会员权限
-日期:2022.11.19
-[rewrite_local]
-^https?:\/\/(.*.dushu365.com) url script-request-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/fdds.js
-hostname = *.dushu365.com
-**********************************************/
-var body = $response.body.replace(/"token":".*?"/g, '"token":"20221119qbmqdUt8vptKBeZn7ox"'); $done({body});
