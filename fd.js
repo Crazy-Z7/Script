bec5cf6e62bb395c2a3a457840c646ef66eb8d95
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-11-19 17:34:08 +0800

    Delete fd.js

--- fd.js
+++
@@ -1,8 +0,0 @@
-/**********************************************
-@@Zoo
-樊登读书解锁会员权限
-日期:2022.11.19
-[rewrite_local]
-^https?:\/\/.+dushu.+(v101/content|book/v100/info|/play/duration|/v100/index).*$ url request-body "token":"[^"]+ request-body "token":"20221119ZCN6g6tVR0qxjpmCXTV
-hostname = *dushu*
-**********************************************/
