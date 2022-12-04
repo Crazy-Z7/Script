ba2758fb2ca3dc5822cf91a8a5664b95492d1d16
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-12-04 21:57:16 +0800

    Update Cycles.js

--- Cycles.js
+++ Cycles.js
@@ -9,10 +9,4 @@ hostname = pcbe.perigee.se
 **************************************/
 
 
-var ojbk = JSON.parse($response.body);
-   
-    ojbk = var ojbk = JSON.parse($response.body);
-   
-    ojbk = {   "has_valid_purchases" : true,   "subscription_purchases_state" : 3909316327,   "subscription_purchases" : [     {       "period" : "P1Y",       "expires_at" : {         "offset" : 0,         "timestamp" : 9990764138       },       "sku_identifier" : "Premium_B_12Months",       "purchased_at" : {         "offset" : 0,         "timestamp" : 1670159338       },       "cancelled_at" : {         "offset" : 0,         "timestamp" : 1670159556       },       "sandbox" : false,       "auto_renew" : false,       "trial_period" : "P1W",       "ownership" : "purchased",       "grace_period_days" : 0,       "status" : "normal",       "store" : "app_store"     }   ],   "in_app_purchases" : [    ],   "buyer_identifier" : "60f9751e-4aeb-433b-a1f7-6a3c92a39536" }
-
-$done({body : JSON.stringify(ojbk)});
+var body = $response.body.replace(/.+/g, '{   "has_valid_purchases" : true,   "subscription_purchases_state" : 3909316327,   "subscription_purchases" : [     {       "period" : "P1Y",       "expires_at" : {         "offset" : 0,         "timestamp" : 9670764138       },       "sku_identifier" : "Premium_B_12Months",       "purchased_at" : {         "offset" : 0,         "timestamp" : 1670159338       },       "cancelled_at" : {         "offset" : 0,         "timestamp" : 1670159556       },       "sandbox" : false,       "auto_renew" : false,       "trial_period" : "P1W",       "ownership" : "purchased",       "grace_period_days" : 0,       "status" : "normal",       "store" : "app_store"     }   ],   "in_app_purchases" : [    ],   "buyer_identifier" : "60f9751e-4aeb-433b-a1f7-6a3c92a39536" }'); $done({body});
