1fbb9f6a0ae7c00324f4722570de2404a8b2f8c2
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-12-04 20:07:16 +0800

    Create hupu.js

---
+++ hupu.js
@@ -0,0 +1,12 @@
+/*************************************************
+@@Zoo
+日期:2022.12.04
+[rewrite_local]
+^https?:\/\/.*.hupu.com url script-request-header https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/hupu.js
+hostname = *.hupu.com
+*********************************************/
+var hupuad = $request.headers;
+hupuad['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148';
+  $done({
+    headers: hupuad
+});
