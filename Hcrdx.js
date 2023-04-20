bbe2f9cfab63a61ee67d73b860e51cc0d08ab0ea
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2023-04-20 19:01:58 +0800

    Update Hcrdx.js

--- Hcrdx.js
+++ Hcrdx.js
@@ -7,7 +7,8 @@
 hostname = hcrdxgame.dcatgame.com
 **********************************************/
 var obj = JSON.parse($response.body);
-obj.data.currentEnergy = 555555555555555555665;
+
+obj.data.currentEnergy = 555555555555555555555;
 obj.data.timeSandCount = "4444444444444444444";
 obj.data.redDiamondCount = “11111111111111111”;
 obj.data.mysticalRockCount = “222222222222222”;
