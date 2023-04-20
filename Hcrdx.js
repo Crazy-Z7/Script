ab962d85ec5eaef9fbf807a22f36148bf636d984
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2023-04-20 18:55:14 +0800

    Update Hcrdx.js

--- Hcrdx.js
+++ Hcrdx.js
@@ -3,14 +3,13 @@
 小程序火柴人大侠破解
 日期:2023.04.20
 [rewrite_local]
-^https？:\/\/hcrdxgame\.dcatgame\.com\/data-manager\/login?mcachenum=\d+ url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/Hcrdx.js
+^https？:\/\/hcrdxgame\.dcatgame\.com\/data-manager\/login url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/Hcrdx.js
 hostname = hcrdxgame.dcatgame.com
 **********************************************/
 var obj = JSON.parse($response.body);
-obj.data.rankScore = 88888888888888;
-obj.data.redDiamondCount = 4092599349000;
-obj.data.timeSandCount = “888888888888888”;
-obj.data.mysticalRockCount = “77777777778888”;
-obj.data.summonEAdCount = “9999999999999999”;
+obj.data.currentEnergy = 888888888888888888888;
+obj.data.timeSandCount = "4092599349000000000";
+obj.data.redDiamondCount = “11111111111111111”;
+obj.data.mysticalRockCount = “222222222222222”;
+obj.data.summonEAdCount = “333333333333333333”;
 $done({body : JSON.stringify(obj)});
-
