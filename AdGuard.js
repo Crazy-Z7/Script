2236a249a757428d569efebd0eda72913fe1295c
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2023-05-18 21:53:21 +0800

    Update AdGuard.js

--- AdGuard.js
+++ AdGuard.js
@@ -1,1 +1,27 @@
+/**********************************************
+@Zoo
+美区AdGuard解锁永久订阅，规则只需开一次
+日期:2023.05.18
+[rewrite_local]
+^https?:\/\/mobile-api\.adguard\.org\/api\/2\.0\/ios_validate_receipt\/ADG_EXT url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/AdGuard.js
+hostname = mobile-api.adguard.org
+**********************************************/
+ 
+
+
+
+
+var OwnerReadGuard = JSON.parse($response.body);
+   
+OwnerReadGuard = {
+  "products" : [
+    {
+      "premium_status" : "ACTIVE",
+      "product_id" : "com.adguard.lifetimePurchase"
+    }
+  ]
+}
+
+
+$done({body : JSON.stringify(ojbk)});
 
