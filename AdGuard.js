73cfdb9e7e9cf443f0f403e24a94e58643037aef
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2023-06-01 13:39:59 +0800

    Update AdGuard.js

--- AdGuard.js
+++ AdGuard.js
@@ -3,7 +3,7 @@
 美区AdGuard解锁永久订阅，规则只需开一次
 日期:2023.05.18
 [rewrite_local]
-^https?:\/\/mobile-api\.adguard\.org\/api\/2\.0\/ios_validate_receipt\/ADG_EXT url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/AdGuard.js
+^https?:\/\/mobile-api\.adguard\.org\/api\/2\.0\/ios_validate_receipt\/ADG_EXT url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Script/main/AdGuard.js
 hostname = mobile-api.adguard.org
 **********************************************/
  
