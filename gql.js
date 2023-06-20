31a2edc181d60cb3f0e65f78cc5cfce46b3c65c4
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2023-06-21 00:28:58 +0800

    更新 gql.js

--- gql.js
+++ gql.js
@@ -1,10 +1,14 @@
 /**********************************************
 作者:Zoo
 过期啦解锁永久订阅
-日期:2023.06.20
+日期:2023.06.21
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com/v\d/(subscribers/(\$RCAnonymousID\%)?(\w)+|receipts)$ url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Script/main/gql.js
-hostname = api.revenuecat.com
+https://api.guoqi365.com/1.1/functions/getUserInfo url response-body "expires_date":"\d+" response-body "expires_date":"4092622536000"
+https://api.revenuecat.com/v1/subscribers/identify url response-body .+ response-body {   "request_date_ms" : 1687277100657,   "request_date" : "2023-06-20T16:05:00Z",   "subscriber" : {     "non_subscriptions" : {      },     "first_seen" : "2023-06-20T16:03:05Z",     "original_application_version" : null,     "other_purchases" : {      },     "management_url" : null,     "subscriptions" : {      },     "entitlements" : {      },     "original_purchase_date" : null,     "original_app_user_id" : "6491cdb6a6747e45a0d5a429",     "last_seen" : "2023-06-20T16:03:05Z"   } }
+https://api.guoqi365.com/1.1/functions/refreshUserMembership url response-body .+ response-body {   "result" : {     "message" : "success",     "data" : {       "status" : 1,       "is_pro" : true,       "membership_name" : "yearly",       "membership_type" : 2,       "expires_date" : "2099-09-09 T15:35:36.000Z",       "member_id" : "$RCAnonymousID:d7c655b3d6084f8395c3b54cf180492d",       "user_id" : "6491c6e0bb86895f28a91a83"     },     "code" : 1   } }
+
+hostname = api.revenuecat.com,api.guoqi365.com
 **********************************************/
 let Zoo = JSON.parse($response.body);
    
