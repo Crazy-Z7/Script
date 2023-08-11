776c6e72b893385d408814605d28da89241ca042
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2023-08-11 22:55:53 +0800

    更新 Aptv.js

--- Aptv.js
+++ Aptv.js
@@ -6,42 +6,37 @@ Aptv解锁永久订阅
 ^https?:\/\/api\.revenuecat\.com/v\d/(subscribers/(\$RCAnonymousID\%)?(\w)+|receipts)$ url script-echo-response https://raw.githubusercontent.com/Crazy-Z7/Script/main/Aptv.js
 hostname = api.revenuecat.com
 **********************************************/
- "request_date_ms" : 1662904006346,
-   "request_date" : "2022-09-11T13:46:46Z",
-   "subscriber" : {
-     "last_seen" : "2022-09-11T13:44:09Z",
-     "first_seen" : "2022-09-11T13:44:09Z",
-     "original_application_version" : "70",
-     "other_purchases" : {
- 
-     },
-     "management_url" : "https://apps.apple.com/account/subscriptions",
-     "subscriptions" : {
-       "com.kimen.aptvpro.lifetime" : {
-         "is_sandbox" : false,
-         "ownership_type" : "PURCHASED",
-         "billing_issues_detected_at" : null,
-         "period_type" : "normal",
-         "expires_date" : null,
-         "grace_period_expires_date" : null,
-         "unsubscribe_detected_at" : null,
-         "original_purchase_date" : "2022-09-11T13:45:07Z",
-         "purchase_date" : "2022-09-11T13:45:06Z",
-         "store" : "app_store"
-       }
-     },
-     "entitlements" : {
-       "pro" : {
-         "expires_date" : null,
-         "purchase_date" : "2022-09-11T13:45:06Z",
-         "product_identifier" : "com.kimen.aptvpro.lifetime",
-         "grace_period_expires_date" : null
-       }
-     },
-     "original_purchase_date" : "2022-09-11T13:29:47Z",
-     "original_app_user_id" : "6666666666666666666666666666666",
-     "non_subscriptions" : {
- 
-     }
-   }
- }
+   "request_date_ms": 1691684430913,
+  "request_date": "2023-08-10T16:20:30Z",
+  "subscriber": {
+    "non_subscriptions": {
+    },
+    "first_seen": "2023-08-10T16:15:27Z",
+    "original_application_version": "1",
+    "other_purchases": {
+    },
+    "management_url": null,
+    "subscriptions": {
+      "com.kimen.aptvpro.lifetime": {
+        "store": "app_store",
+        "purchase_date": "2023-02-23T02:33:33Z",
+        "ownership_type": "PURCHASED",
+        "original_purchase_date": "2023-02-23T02:33:33Z",
+        "expires_date": "6666-06-06T06:06:06Z"
+      }
+    },
+    "entitlements": {
+      "pro": {
+        "store": "app_store",
+        "purchase_date": "2023-02-23T02:33:33Z",
+        "ownership_type": "PURCHASED",
+        "original_purchase_date": "2023-02-23T02:33:33Z",
+        "product_identifier": "com.kimen.aptvpro.lifetime",
+        "expires_date": "6666-06-06T06:06:06Z"
+      }
+    },
+    "original_purchase_date": "2022-10-02T04:32:28Z",
+    "original_app_user_id": "$RCAnonymousID:807d56e7d09e4e1f8cfa7e50cd323c6b",
+    "last_seen": "2023-08-10T16:15:27Z"
+  }
+}
