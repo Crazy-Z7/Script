e00c06721907195a3f5de5b8fbc22d5820049732
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2023-08-11 23:23:41 +0800

    更新 aptv.js

--- aptv.js
+++ aptv.js
@@ -3,7 +3,7 @@
 Aptv解锁永久订阅
 日期:2023.08.11
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com/v\d/(subscribers/(\$RCAnonymousID\%)?(\w)+|receipts)$ url script-echo-response https://raw.githubusercontent.com/Crazy-Z7/Script/main/aptv.js.js
+^https?:\/\/api\.revenuecat\.com/v\d/(subscribers/(\$RCAnonymousID\%)?(\w)+|receipts)$ url script-echo-response https://raw.githubusercontent.com/Crazy-Z7/Script/main/aptv.js
 hostname = api.revenuecat.com
 **********************************************/ 
 let Zoo = {
