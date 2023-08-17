f9333e3348d63e8b008a472d1b9507cc2a0ace03
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2023-08-17 21:55:47 +0800

    更新 Httpbot.js

--- Httpbot.js
+++ Httpbot.js
@@ -1,6 +1,7 @@
 /**********************************************
 作者:Zoo
 Httpbot解锁永久订阅,进软件恢复购买
+强大的网络调试工具，类似Anubis
 日期:2023.08.17
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com/v\d/(subscribers/(\$RCAnonymousID\%)?(\w)+|receipts)$ url script-echo-response https://raw.githubusercontent.com/Crazy-Z7/Script/main/Httpbot.js
